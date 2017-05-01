4622a8b9cb9fb812215221d7e35130e443e0d91b
Panachai <panachai.ny@gmail.com>
2017-05-01 20:51:11 +0700

    complete Dice

--- TrainSolution4_2/Program.cs
+++ TrainSolution4_2/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace TrainSolution4_2
 {
@@ -10,6 +9,30 @@ namespace TrainSolution4_2
     {
         static void Main(string[] args)
         {
+            int mark = 0, i = 1; ;          //กำหนด ค่าเริ่มต้น ของตัวแปร
+            Dice dic1 = new Dice();     // สร้าง อินสแตนด์ dic1 สำหรับสุ่มตัวเลขการโยนลูกเต๋า
+            while (mark != 1)           //ให้วนจนกว่าการโยนลูกเต๋าจะได้เลขไม่เท่ากับ 1 (ค่าเริ่มต้น 0 )
+            {                   // หลังจากการวนรอบจะได้ค่า 1- 6 ถ้าได้เลข 1 จะหยุดวนรอบ
+                mark = dic1.Throw();        //เก็บค่าการโยนลูกเต๋าไว้ในตัวแปร mark
+                Console.WriteLine(" {0} Mark of Dice : {1} ", i, mark); //แสดงจำนวนครั้ง (i) และแต้ม(mark)
+                System.Threading.Thread.Sleep(10);  // หน่วงเวลาไว้เพื่อไม่ให้แต้มซ้ำกันในขณะสุ่มตัวเลข
+                i = i + 1;                  // เพิ่มจำนวนครั้งที่โยนลูกเต๋า
+            }
+            Console.WriteLine("Throw of dice = 1 is : " + (i - 1) + " time "); //สรุปการโยนลูกเต๋าให้เท่ากับ 1 ว่ากี่ครั้ง
+            Console.ReadLine();
+        }
+    }
+    class Dice
+    {
+        Random random = new Random();
+        int[] resultDice = new[] { 1, 2, 3, 4, 5, 6 };
+
+        internal int Throw()
+        {
+            //throw new NotImplementedException();
+            int valueRandom = random.Next(0, resultDice.Length) + 1;
+            //Console.WriteLine(valueRandom);
+            return valueRandom;
         }
     }
 }
